04cd5c38c32b6a7859437d0fa235138a3220149d
soohyeon.won <soohyun.won@kbfg.com>
2023-01-16 12:59:27 +0900

    [Study][DP☀️][Prototype] 프로토타입 장점/단점

--- RxSwiftStudyInUIkit/Study/DesignPatterns/Create/Prototype/PrototypeViewController.swift
+++ RxSwiftStudyInUIkit/Study/DesignPatterns/Create/Prototype/PrototypeViewController.swift
@@ -31,6 +31,14 @@ final class PrototypeViewController: UIViewController {
         
         JAVA에서 cloneable을 제공
         shallow copy(얕은 복사) <-> deep copy
+        
+        장점
+        복잡한 객체를 만드는 과정을 숨길 수 있다.
+        기존 객체를 복제하는 과정이 새 인스턴스를 만드는것보다 비용(시간 or 메모리)적인 면에서 효율적일 수도있다.
+        추상적인 타입을 리턴할 수 있다.
+        
+        단점
+        복제한 객체를 만드는 과정 자체가 복잡할 수 있다.(특히, 순환 참조가 있는 경우)
         """
         
         client()
