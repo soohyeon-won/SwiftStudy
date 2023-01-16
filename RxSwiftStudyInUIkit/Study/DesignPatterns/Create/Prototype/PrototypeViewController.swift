d74444ce87c90b134b4782a3e949640836f459d0
soohyeon.won <soohyun.won@kbfg.com>
2023-01-16 12:57:33 +0900

    [Study][DP☀️][Prototype] 프로토타입 패턴 적용하기

--- RxSwiftStudyInUIkit/Study/DesignPatterns/Create/Prototype/PrototypeViewController.swift
+++ RxSwiftStudyInUIkit/Study/DesignPatterns/Create/Prototype/PrototypeViewController.swift
@@ -28,13 +28,37 @@ final class PrototypeViewController: UIViewController {
         기존 인스턴스를 복제하여 새로운 인스턴스를 만드는 방법
         
         복제 기능을 갖추고 있는 기존 인스턴스를 프로토타입으로 사용해 새 인스턴스를 만들 수 있다.
+        
+        JAVA에서 cloneable을 제공
+        shallow copy(얕은 복사) <-> deep copy
         """
         
         client()
     }
     
     private func client() {
+        let repository = GithubRepository()
         
+        let githubIssue = GithubIssue(repository: repository)
+        
+        let clone = githubIssue
+        
+        // shallow copy의 경우
+        print(clone.repository === githubIssue.repository)
+        
+        // java에서는 clonable을 제공하지만 swift X
+    }
+}
+
+final class GithubRepository {
+    
+}
+
+final class GithubIssue {
+    let repository: GithubRepository
+    
+    init(repository: GithubRepository) {
+        self.repository = repository
     }
 }
 
