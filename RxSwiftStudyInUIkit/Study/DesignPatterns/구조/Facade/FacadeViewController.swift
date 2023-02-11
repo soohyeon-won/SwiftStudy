545b856a4285eaf59e50a72804476637afb90b56
soohyeon-won <main.async@kakao.com>
2023-02-11 15:26:10 +0900

    [Study][DP☀️][Facade] 퍼사드 패턴: 패턴 소개

--- RxSwiftStudyInUIkit/Study/DesignPatterns/구조/Facade/FacadeViewController.swift
+++ RxSwiftStudyInUIkit/Study/DesignPatterns/구조/Facade/FacadeViewController.swift
@@ -25,6 +25,12 @@ final class FacadeViewController: UIViewController {
         
         textView.text = """
         [ 퍼사드 패턴 ]
+        건물의 외관, 입구쪽을 바라본 전경
+        건물 안에 뭐가 있는지는 알 수 없음
+        유연한 개발을 위해 약결합 시스템(Loosely Coupled System) 에 대해 생각해보아야 함
+        
+        클라이언트가 사용해야 하는 복잡한 서브 시스템 의존성을 간단한 인터페이스로 추상화 할 수 있다.
+        복잡한 기능은 퍼사드 뒤로 숨김
         
         [장점]
         
