e60cf124591540d5d100a64254e6df7da74c2186
soohyeon-won <main.async@kakao.com>
2023-02-12 21:27:28 +0900

    [Study][DP☀️][Flyweight] 플라이웨이트 패턴: 패턴 소개

--- RxSwiftStudyInUIkit/Study/DesignPatterns/구조/Flyweight/FlyweightViewController.swift
+++ RxSwiftStudyInUIkit/Study/DesignPatterns/구조/Flyweight/FlyweightViewController.swift
@@ -25,6 +25,14 @@ final class FlyweightViewController: UIViewController {
         
         textView.text = """
         [ 플라이웨이트 패턴 ]
+        자주 변하는 속성(또는 외적인 속성 extrinsic)과 변하지 않는 속성(또는 내적인 속성, intrinsic)을 분리하고 재사용하여 메모리 사용을 줄일 수 있다.
+        다른 객체들이 공통으로 사용하는 데이터를 캐싱하여 RAM을 절약합니다.
+        
+        성능을 개선하는 패턴
+        자주 변하지 않는 속성을 재사용하여 사용
+        extrinsic(외적, 자주변하는것, 본질적이지 않은것) <-> intrinsic(내적, 변하지않는것, 본질적인 것)
+        FliweightFactory
+        Fliyweight
         
         [장점]
         
