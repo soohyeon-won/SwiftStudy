32b9536704a806d70c4538f5dd056ae06fb0f0b5
soohyeon-won <main.async@kakao.com>
2023-02-12 21:41:20 +0900

    [Study][DP☀️][Proxy] 프록시 패턴: 패턴 소개

--- RxSwiftStudyInUIkit/Study/DesignPatterns/구조/Proxy/ProxyViewController.swift
+++ RxSwiftStudyInUIkit/Study/DesignPatterns/구조/Proxy/ProxyViewController.swift
@@ -25,6 +25,18 @@ final class ProxyViewController: UIViewController {
         
         textView.text = """
         [ 프록시 패턴 ]
+        특정 객체에 대한 접근을 제어하거나 기능을 추가할 수 있는 패턴
+        - 초기화 지연, 접근 제어, 로깅, 캐싱 등 다양하게 응용해 사용할 수 있다.
+        
+        Subject
+        ↑
+        ↑ ←-------------
+        RealSubject ← proxy
+        
+        대리, 대리인
+        security를 적용해서 접근을 제한 할 수 있는 로직을 넣을 수 도 있고
+        로깅, 캐싱을 적용할 수 도 있음.
+        
         
         [장점]
         
@@ -36,5 +48,13 @@ final class ProxyViewController: UIViewController {
     }
     
     private func client() {
+        GameService().startGame()
+    }
+}
+
+class GameService {
+    
+    func startGame() {
+        print("이 자리에 오신 여러분을 진심으로 환영합니다.")
     }
 }
