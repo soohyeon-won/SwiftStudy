b3d51e45de00346e1a1f433585b91856abc29d6d
soohyeon.won <soohyun.won@kbfg.com>
2023-02-13 12:23:52 +0900

    [Study][DP☀️][Proxy] 프록시 패턴: 예제

--- RxSwiftStudyInUIkit/Study/DesignPatterns/구조/Proxy/ProxyViewController.swift
+++ RxSwiftStudyInUIkit/Study/DesignPatterns/구조/Proxy/ProxyViewController.swift
@@ -48,13 +48,52 @@ final class ProxyViewController: UIViewController {
     }
     
     private func client() {
-        GameService().startGame()
+//        DefaultGameService().startGame()
+//
+//        GameServiceProxy().startGame()
+        
+        GameServiceProxy(gameService: DefaultGameService()).startGame()
+        
+        GameServiceProxy2().startGame()
     }
 }
 
-class GameService {
+protocol GameService {
+    func startGame()
+}
+
+class DefaultGameService: GameService {
     
     func startGame() {
         print("이 자리에 오신 여러분을 진심으로 환영합니다.")
+    }
+}
+
+class GameServiceProxy: GameService {
+    
+    private let gameService: GameService
+    
+    init(gameService: GameService) {
+        self.gameService = gameService
+    }
+    
+    func startGame() {
+        let log = Date() //부가적인 일들은 프록시에서 제공
+        gameService.startGame()
+        print("log", log)
+    }
+}
+
+class GameServiceProxy2: GameService {
+    
+    var gameService: GameService? = nil
+    
+    func startGame() {
+        let log = Date() //부가적인 일들은 프록시에서 제공
+        if gameService == nil {
+            gameService = DefaultGameService() // lazy init제공 가능
+        }
+        gameService?.startGame()
+        print("log", log)
     }
 }
