4dc4c3f9ce3117a259096a4b957d71c2e6b376d4
soohyeon.won <soohyun.won@kbfg.com>
2023-02-24 12:40:33 +0900

    [Study][DP☀️][Command] 커맨드 패턴: 패턴 소개

--- RxSwiftStudyInUIkit/Study/DesignPatterns/Behavioral/Command/CommandViewController.swift
+++ RxSwiftStudyInUIkit/Study/DesignPatterns/Behavioral/Command/CommandViewController.swift
@@ -25,6 +25,16 @@ final class CommandViewController: UIViewController {
         
         textView.text = """
         [ 커맨드 패턴 ]
+        요청을 캡슐화 하여 호출자(invoker)와 수신자(receiver)를 분리하는 패턴.
+        - 요청을 처리하는 방법이 바뀌더라도 호출자의 코드는 변경되지 않는다.
+        
+        Invoker -> Command(interface) + excute()
+                     ⬆️
+        receiver <- ConcreateCommand + excute
+        
+        아래 예제에서는
+        invoker / receiver
+        Button / Light
         
         [장점]
         
@@ -37,6 +47,29 @@ final class CommandViewController: UIViewController {
     }
     
     private func client() {
+        
+    }
+}
+
+final class Button {
+    private let light: Light
+    
+    public init(light: Light) {
+        self.light = light
+    }
+    
+    func press() {
+        light.off()
+    }
+    
+}
+
+struct Light {
+    func on() {
+        
+    }
+    
+    func off() {
         
     }
 }
