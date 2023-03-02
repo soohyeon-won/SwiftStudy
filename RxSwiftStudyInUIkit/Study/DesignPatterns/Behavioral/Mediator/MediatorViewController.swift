d1d3a0c55e91a75ec0af7d32815ef77ae7c278bc
soohyeon.won <soohyun.won@kbfg.com>
2023-03-02 12:39:09 +0900

    [Study][DP☀️][Mediator] 중재자 패턴: 패턴 소개

--- RxSwiftStudyInUIkit/Study/DesignPatterns/Behavioral/Mediator/MediatorViewController.swift
+++ RxSwiftStudyInUIkit/Study/DesignPatterns/Behavioral/Mediator/MediatorViewController.swift
@@ -25,12 +25,30 @@ final class MediatorViewController: UIViewController {
         
         textView.text = """
         [ 중재자 패턴 ]
+        여러 객체들이 소통하는 방법을 캡슐화하는 패턴
+        - 여러 컴포넌트간의 결합도를 중재자를 통해 낮출 수 있다.
+        
+        [구조도]
+        Mediator(interface) <- Colleague
+        ↑                       ↑    ↑
+        ConcreateMediator ColleagueA ColleagueB
+        
+        Colleague가 Colleagu를 참조하지는 않는다.
+        무조건 구조도를 맞추어서 사용하라는 법칙은 없음
+        모든 Colleague가 공통의 Interface를 상속받지 않을 수 있고 기타 등등
+        다른 방식으로 만들 수 있음.
+        
+        - 실생활 예시
+        아파트 민원 > 관리실 > 주민들에게 공지
+        A비행기 > 관제탑 > B비행기
         
         [장점]
         
         [단점]
         
         [사용 예제]
+        before예제 코드에서는 Guest가 cleaningService를 직접 호출하고 있음
+        
         """
         
         client()
@@ -39,4 +57,31 @@ final class MediatorViewController: UIViewController {
     private func client() {
         
     }
+}
+
+final class CleaningService {
+    
+    func clean(gym: Gym) {
+        print("clean up gym: \(gym)")
+    }
+    
+    func getTower(guest: Guest, numberOfTower: Int) {
+        print("numberOfTower: \(numberOfTower) towers to \(guest)")
+    }
+    
+    func clean(restaurant: Restaurant) {
+        print("clean up restaurant: \(restaurant)")
+    }
+}
+
+class Gym {
+    
+}
+
+class Guest {
+    
+}
+
+class Restaurant {
+    
 }
