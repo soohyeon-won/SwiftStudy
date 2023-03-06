8387d3851295caed152a12f9f80d8457aecc2774
soohyeon-won <main.async@kakao.com>
2023-03-06 23:24:14 +0900

    [Study][DP☀️][Observer] 상태 패턴: 패턴 적용하기(1) Before코드

--- RxSwiftStudyInUIkit/Study/DesignPatterns/Behavioral/State/StateViewController.swift
+++ RxSwiftStudyInUIkit/Study/DesignPatterns/Behavioral/State/StateViewController.swift
@@ -39,6 +39,140 @@ final class StateViewController: UIViewController {
     }
     
     private func client() {
+        example1_vendingMachine()
+    }
+    
+    private func example1_vendingMachine() {
+        let vendingMachine = VendingMachine.shared
+
+        vendingMachine.insertCoin(100) // 동전을 삽입했습니다.
+        vendingMachine.pressButton()   // 동전이 없습니다.
+
+        vendingMachine.insertCoin(100) // 동전을 삽입했습니다.
+        vendingMachine.pressButton()   // 음료수가 나왔습니다.
+    }
+}
+
+public class client {
+    func client() {
+        let student = Student (name: "whiteship")
+        let onlineCourse = OnlineCourse()
+        let keesun = Student(name: "keesun")
         
+        keesun.addPrivateCourse(onlineCourse)
+        onlineCourse.addStudent(student)
+        onlineCourse.changeState(OnlineCourse.State.PRIVATE)
+        onlineCourse.addStudent(keesun)
+        onlineCourse.addReview(review: "hello", student)
+        
+        print(onlineCourse.getState())
+        print(onlineCourse.getStudents())
+        print(onlineCourse.getReviews())
+    }
+}
+
+// MARK: - 수강신청
+class Student {
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+
+    func addPrivateCourse(_ state: OnlineCourse) {
+
+    }
+}
+
+class OnlineCourse {
+    
+    enum State {
+        case PRIVATE
+    }
+    
+    func addStudent() {
+        
+    }
+    
+    func changeState(_ state: State) {
+        
+    }
+    
+    func addStudent(_ student: Student) {
+        
+    }
+    
+    func addReview(review: String, _ student: Student) {
+        
+    }
+    
+    func getState() {
+        
+    }
+    
+    func getStudents() {
+        
+    }
+    
+    func getReviews() {
+        
+    }
+}
+
+// MARK: - 자판기
+// 자판기 상태 프로토콜
+protocol VendingMachineState {
+    func insertCoin(_ amount: Int)
+    func pressButton()
+}
+
+// 동전 없는 상태
+class NoCoinState: VendingMachineState {
+    func insertCoin(_ amount: Int) {
+        print("동전을 삽입했습니다.")
+        // 상태 변경
+        VendingMachine.shared.transitionToState(.coinInserted)
+    }
+    
+    func pressButton() {
+        print("동전이 없습니다.")
+    }
+}
+
+// 동전 삽입된 상태
+class CoinInsertedState: VendingMachineState {
+    func insertCoin(_ amount: Int) {
+        print("이미 동전이 삽입되어 있습니다.")
+    }
+    
+    func pressButton() {
+        print("음료수를 나왔습니다.")
+        // 상태 변경
+        VendingMachine.shared.transitionToState(.noCoin)
+    }
+}
+
+// 자판기 객체
+class VendingMachine {
+    static let shared = VendingMachine()
+    private var state: VendingMachineState
+    
+    // 초기 상태: 동전 없음
+    private init() {
+        self.state = NoCoinState()
+    }
+    
+    // 상태 변경 함수
+    func transitionToState(_ state: VendingMachineState) {
+        self.state = state
+    }
+    
+    // 동전 삽입 함수
+    func insertCoin(_ amount: Int) {
+        state.insertCoin(amount)
+    }
+    
+    // 버튼 누르기 함수
+    func pressButton() {
+        state.pressButton()
     }
 }
