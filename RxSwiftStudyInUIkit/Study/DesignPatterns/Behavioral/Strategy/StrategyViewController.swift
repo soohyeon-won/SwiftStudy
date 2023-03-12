6a2eca24f1b8cf8146994195b637af9cfac75bd4
soohyeon-won <main.async@kakao.com>
2023-03-12 16:30:16 +0900

    [Study][DP☀️][Strategy] 전략 패턴: 패턴 소개

--- RxSwiftStudyInUIkit/Study/DesignPatterns/Behavioral/Strategy/StrategyViewController.swift
+++ RxSwiftStudyInUIkit/Study/DesignPatterns/Behavioral/Strategy/StrategyViewController.swift
@@ -24,7 +24,9 @@ final class StrategyViewController: UIViewController {
         }
         
         textView.text = """
-        [ 전략 패턴 ]
+        [ 전략 (Strategy) 패턴 ]
+        - 여러 알고리듬을 캡슐화하고 상호 교환 가능하게 만드는 패턴.
+        • 컨텍스트에서 사용할 알고리듬을 클라이언트 선택한다.
         
         [장점]
         
