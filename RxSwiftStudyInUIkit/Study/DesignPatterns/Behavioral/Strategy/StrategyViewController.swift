0a0235980ced2e9bd64c43171165d1f1acee5c27
Soohyeon won <30689094+soohyeon-won@users.noreply.github.com>
2023-03-12 16:54:37 +0900

    [Study][DP☀️][Strategy] 전략 패턴 (#33)
    * [Study][DP☀️][Strategy] 전략 패턴: 패턴 소개
    
    * [Study][DP☀️][Strategy] 전략 패턴: 패턴 적용하기
    
    * [Study][DP☀️][Strategy] 전략 패턴: 장점, 단점
    
    * [Study][DP☀️][Strategy] 전략 패턴: Swift에서 사용하는 전략패턴

--- RxSwiftStudyInUIkit/Study/DesignPatterns/Behavioral/Strategy/StrategyViewController.swift
+++ RxSwiftStudyInUIkit/Study/DesignPatterns/Behavioral/Strategy/StrategyViewController.swift
@@ -24,19 +24,91 @@ final class StrategyViewController: UIViewController {
         }
         
         textView.text = """
-        [ 전략 패턴 ]
+        [ 전략 (Strategy) 패턴 ]
+        - 여러 알고리듬을 캡슐화하고 상호 교환 가능하게 만드는 패턴.
+        • 컨텍스트에서 사용할 알고리듬을 클라이언트 선택한다.
+        - 객체가 특정 동작을 수행하는 데 있어 다양한 전략을 가질 수 있을 때, 동적으로 전략을 선택하여 사용하는 디자인 패턴
         
         [장점]
+        • 새로운 전략을 추가하더라도 기존 코드를 변경하지 않는다.
+        • 상속 대신 위임을 사용할 수 있다.
+        • 런타임에 전략을 변경할 수 있다.
         
         [단점]
+        • 복잡도가 증가한다.
+        • 클라이언트 코드가 구체적인 전략을 알아야 한다.
         
         [사용 예제]
+        - 신호등 시스템
+        - 결제 시스템
+        
+        [추가 의견 by ChatGPT]
+        Swift 표준 라이브러리에는 전략 패턴을 활용하여 구현되어 제공되는 클래스나 함수는 없습니다. 그러나 Swift에서 전략 패턴을 구현하는 것은 간단하며, 프로토콜과 클래스를 사용하여 쉽게 구현할 수 있습니다.
+
+        Swift에서 전략 패턴을 활용하여 구현된 라이브러리나 함수가 있다면, 해당 라이브러리나 함수가 전략 패턴을 활용한 것일 뿐, 전략 패턴 자체를 구현한 것은 아닙니다. 따라서 전략 패턴을 이해하고 활용할 수 있다면, Swift에서 자유롭게 전략 패턴을 구현하여 사용할 수 있습니다.
         """
         
         client()
     }
     
     private func client() {
-        
+        let creditCardPayment = CreditCardPaymentStrategy(cardNumber: "1234-5678-9101-1121", expirationDate: "05/25", cvv: "123")
+        let payPalPayment = PayPalPaymentStrategy(email: "example@gmail.com", password: "password")
+
+        let productPurchaseWithCreditCard = ProductPurchase(paymentStrategy: creditCardPayment)
+        productPurchaseWithCreditCard.processPayment(amount: 50.0) // "50.0 달러를 신용카드로 결제했습니다."
+
+        let productPurchaseWithPayPal = ProductPurchase(paymentStrategy: payPalPayment)
+        productPurchaseWithPayPal.processPayment(amount: 100.0) // "100.0 달러를 페이팔로 결제했습니다."
+    }
+}
+
+// 전략 프로토콜
+protocol PaymentStrategy {
+    func pay(amount: Double)
+}
+
+// 신용카드 결제 전략
+class CreditCardPaymentStrategy: PaymentStrategy {
+    private let cardNumber: String
+    private let expirationDate: String
+    private let cvv: String
+    
+    init(cardNumber: String, expirationDate: String, cvv: String) {
+        self.cardNumber = cardNumber
+        self.expirationDate = expirationDate
+        self.cvv = cvv
+    }
+    
+    func pay(amount: Double) {
+        print("\(amount) 달러를 신용카드로 결제했습니다.")
+    }
+}
+
+// 페이팔 결제 전략
+class PayPalPaymentStrategy: PaymentStrategy {
+    private let email: String
+    private let password: String
+    
+    init(email: String, password: String) {
+        self.email = email
+        self.password = password
+    }
+    
+    func pay(amount: Double) {
+        print("\(amount) 달러를 페이팔로 결제했습니다.")
+    }
+}
+
+// 상품 구매 클래스
+class ProductPurchase {
+    private let paymentStrategy: PaymentStrategy
+    
+    init(paymentStrategy: PaymentStrategy) {
+        self.paymentStrategy = paymentStrategy
+    }
+    
+    func processPayment(amount: Double) {
+        paymentStrategy.pay(amount: amount)
     }
 }
