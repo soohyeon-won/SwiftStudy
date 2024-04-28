17106e2134bfaaab6617f435f858d5f9e752bd03
soohyeon-won <main.async@kakao.com>
2024-04-28 19:50:46 +0900

    [combine] just, future, passthroughSubject

--- RxSwiftStudyInUIkit/Study/Combine/Example/PublishersEx1.swift
+++ RxSwiftStudyInUIkit/Study/Combine/Example/PublishersEx1.swift
@@ -14,7 +14,7 @@ struct PublishersEx1View: View {
     let handler: PublishersEx1Handler = .init()
     
     var body: some View {
-        VStack {
+        VStack(spacing: 12) {
             Button {
                 handler.example_just()
             } label: {
@@ -36,8 +36,9 @@ struct PublishersEx1View: View {
     }
 }
 
-struct PublishersEx1Handler {
+class PublishersEx1Handler {
     
+    var cancellables = Set<AnyCancellable>()
     // Just는 단일 값을 즉시 생성하여 전달하는 Publisher입니다. 데이터가 한 번만 발생하며 완료됩니다.
     func example_just() {
         let justPublisher = Just(42) // 42라는 값을 가진 Just Publisher 생성
@@ -48,13 +49,16 @@ struct PublishersEx1Handler {
             } receiveValue: { value in
                 print("Received value: \(value)")
             }
+            .store(in: &cancellables)
     }
     
     // Future는 비동기 작업의 결과를 전달하는 Publisher입니다. 예를 들어 네트워크 요청을 수행하고 결과를 반환할 때 사용합니다.
+    // 네트워크 작업에 유용
+    // Future는 Error 타입을 Failure로 지정할 수 있어 오류 처리에 유용합니다.
     func example_future() {
 //        오류 없이 성공적으로 완료된다는 확신이 있을 때 Never를 사용
         let futurePublisher = Future<String, Never> { promise in
-            DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                 // 1초 후에 비동기 작업을 완료하고 값을 전달
                 promise(.success("Hello, Future!"))
             }
@@ -66,6 +70,7 @@ struct PublishersEx1Handler {
             } receiveValue: { value in
                 print("Received value: \(value)")
             }
+            .store(in: &cancellables)
         
         // 에러가 있는 경우
 //        let futurePublisher2 = Future<String, Error> { promise in
@@ -78,15 +83,18 @@ struct PublishersEx1Handler {
 //        }
     }
     
+    // 여러 Subscriber에 전달할 때 유용합니다.
+    // 데이터를 직접 보내거나 완료 신호를 보내는 등의 제어가 가능합니다.
     func example_passthroughSubject() {
         let subject = PassthroughSubject<String, Never>()
 
-        let subscriber = subject
+        subject
             .sink { completion in
                 print("Completion: \(completion)")
             } receiveValue: { value in
                 print("Received value: \(value)")
             }
+            .store(in: &cancellables)
 
         subject.send("Hello, PassthroughSubject!")
         subject.send("Another value")
