14ddcd531877af09488593f45a502841341cadd1
soohyeon-won <main.async@kakao.com>
2024-05-02 20:11:10 +0900

    [combine] currentValueSubject

--- RxSwiftStudyInUIkit/Study/Combine/Example/PublishersEx1.swift
+++ RxSwiftStudyInUIkit/Study/Combine/Example/PublishersEx1.swift
@@ -53,6 +53,12 @@ struct PublishersEx1View: View {
                     } label: {
                         Text("3. passthroughSubject")
                     }
+                    
+                    Button {
+                        handler.example_currentValueSubject()
+                    } label: {
+                        Text("4. currentValueSubject")
+                    }
                 }
                 .padding(.vertical, 16)
                 .background(
@@ -97,8 +103,14 @@ struct PublishersEx1View: View {
 class PublishersEx1Handler: ObservableObject {
     
     @Published var value: Int = 0
+    let currentValueSubject = CurrentValueSubject<String, Error>("Initial value")
     
     var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        subscribeCurrentValueSubject()
+    }
+    
     // Just는 단일 값을 즉시 생성하여 전달하는 Publisher입니다. 데이터가 한 번만 발생하며 완료됩니다.
     func example_just() {
         let justPublisher = Just(42) // 42라는 값을 가진 Just Publisher 생성
@@ -151,6 +163,7 @@ class PublishersEx1Handler: ObservableObject {
     
     // 여러 Subscriber에 전달할 때 유용합니다.
     // 데이터를 직접 보내거나 완료 신호를 보내는 등의 제어가 가능합니다.
+    // PublishSubject와 유사하다
     func example_passthroughSubject() {
         let subject = PassthroughSubject<String, Never>()
 
@@ -165,6 +178,34 @@ class PublishersEx1Handler: ObservableObject {
         subject.send("Hello, PassthroughSubject!")
         subject.send("Another value")
         subject.send(completion: .finished)
+    }
+    
+    // CurrentValueSubject 는 BehaviorSubject와 유사
+    // 지정된 초기값을 구독자가 알 수 있는 형태
+    private func subscribeCurrentValueSubject() {
+        currentValueSubject
+            .receive(on: DispatchQueue.main) // UI 작업을 실행해야 하는 경우
+            .sink { completion in
+                // 완료 또는 오류 처리
+                print("Completion: \(completion)")
+            } receiveValue: { value in
+                // 값 수신
+                print("Received value: \(value)")
+            }
+            .store(in: &cancellables)
+    }
+    
+    func example_currentValueSubject() {
+        currentValueSubject.send("changed")
+    
+        // currentValueSubject를 Naver로 지정한 경우 failure를 받지 않는다.
+        let error = NSError(domain: "ExampleDomain", code: -1, userInfo: [NSLocalizedDescriptionKey: "An example error occurred"])
+        
+        // completion 방출시 send로 새로운 값을 보내도 구독하지 않음
+        currentValueSubject.send(completion: .failure(error))
+        
+        // 위에서 completion 이 방출 되었으므로 구독이 끊김
+        currentValueSubject.send(completion: .finished)
     }
     
     func example_assign() {
